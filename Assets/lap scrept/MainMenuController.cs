9ef205a2b5980ef8e509cc2f81156e91062b5f78
LayanRabba <layanrabba7@gmail.com>
2026-09-20 12:11:53 +0300

    Add main menu settings and volume controls

--- Assets/lap scrept/MainMenuController.cs
+++ Assets/lap scrept/MainMenuController.cs
@@ -3,9 +3,34 @@ using UnityEngine;
 public class MainMenuController : MonoBehaviour
 {
     public GameObject mainMenuCanvas;
+    public GameObject settingsPanel;
 
     public void StartGame()
     {
         mainMenuCanvas.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void SetVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
